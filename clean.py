2ab12aa02617040c503ed52e8b93f6cb5a666042
McKena Ullman <ullman.kena@gmail.com>
2022-02-15 10:54:24 -0700

    Finished cleaning data

--- clean.py
+++ clean.py
@@ -1,9 +1,10 @@
 from matplotlib.pyplot import close
 import pandas as pd
 from sqlalchemy import false
+from numpy import nan
 # importing pandas 
 
-data = pd.read_csv('artwork_data.csv')
+data = pd.read_csv('artwork_data.csv', low_memory=False)
 # opening artwork and saving it to a variable
 # if there are missing values, it will most likely cause 
 # the columns to be listed as objects
@@ -38,8 +39,35 @@ pd.to_numeric(fulldf.height, errors="coerce")
 fulldf.height = pd.to_numeric(fulldf.height, errors="coerce")
 # I have now changed my actual dataframe with the information 
 #in line 33 
-data.close()
 
+data.loc[data.title.str.contains('\s$', regex=True)]
+# now I have several rows where the title ends in at least one white space
+data.title = data.title.str.strip()
+# strips white space on the entire series; this is not an inplace function and 
+#must be set to a variable in order to change my dataset
+#lstrip strips to the left while Rstrip goes to the right 
+#I could also use 'transform' instead of strip
 
-sample = pd.read_csv("artwork_sample")
-print(sample.head)
+pd.isna(data.loc[:, 'dateText'])
+#we are checking to see if the dateText column are numbers 
+# after importing 'not a number' from numpy, I can use this below with 'replace'
+data.replace({ 'dateText': {'date not known' : nan}})
+# not done inplace, but If I added it, it would change my original data set 
+#data.loc[data.year.notnull() & data.year.astype(str)str.contains('[^0-9']), ["year"]] = nan
+# make sure to include the column name or it will set everything to nan
+data.fillna(value={'depth': 0}, inplace=True)
+# pass a value to fillna that is a dictionary. And in that dictionary we can put the colunns 
+# to fill as the 'keys' and the values in the dictionary are the values put in the 'nan' spots 
+# inplace will change my dataframe 
+
+data.dropna()
+# this will return a new dataframe. It dropped all the rows if any of the values were nan 
+data.dropna(how='all').shape
+# I would rather filter to see if there are any rows that have only NAN values. The shape will tell 
+# me how many rows were dropped 
+data.dropna(thresh=15).shape
+# I could also check my rows and drop any that have over 15 blank (or 'nan') values
+
+data.drop_duplicates(subset=['artist'], keep='first')
+# I want to drop any row that has a duplicate artist and keeping the first item 
+# must do 'inplace' in order to edit the original dataframe 
--- cleaning_sample.py
+++ cleaning_sample.py
@@ -1,3 +1,4 @@
+from curses import COLS
 import pandas as pd
 
 data = pd.read_csv("artwork_sample.csv")
@@ -40,4 +41,62 @@ data["standardized_height"] = norm
 # I didn't want to change this column, but I still want my values visible. So I appended 'norm' to 
 # the columns in this data frame (I couldn't do this with dot notation because pandas doesn't allow it)
 
+data.groupby('artist')
+# this will output a dataframeGroupBy object 
+data.groupby('artist').transform('nunique')
+# this will count the number of unique rows per column grouped by artist
+data.groupby('artist')['height']
+#looks at the height column that is grouped by artist that cretates a SeriesGroupBy that I can also use transform on
+data.groupby('artist')['height'].transform('mean')
+#now this shows me, per artist, the mean of the height column. None of this has transformed my original dataset yet. 
+# I could assign it to a column in my original dataframe by setting it equal to variable such as data['mean_height_by_artist]
 
+
+data.filter(items=['id','artist'])
+#this allows me to create a new dataframe with only two columns
+data.filter(like='artist')
+#I could also filter data using the 'like' param which is case sensitive 
+data.filter(regex="(?i)year")
+# we can filter all year columns now regardless of their case 
+# data can also be filtered by rows
+
+
+data.drop('0')
+# drops the first row in the index
+data.drop(columns=['id'])
+# here I am only dropping the id column, but I could drop multiple columns at once if needed 
+# I could read in the data = pd.read_csv('artwork_sample.csv', usecols=['artist, 'year', 'height'])
+# which will take in an array of just those columns
+
+data.columns.str.lower()
+# this will make all the columns read in a lower case 
+[x.lower() for x in data.columns]
+# this does the same thing which gives us more control because we can add 
+# an element of specificity to this logic 
+#  remember that this does not change the original data set unless I set it equal to a variable 
+
+data.rename(columns={'thumbnailUrl': 'thumbnail'})
+#this renames the column to something more useful which accepts many different arguments. 
+# in the example above, I just took in the original name and output the new name. I would use inplace if I wanted to change original df
+
+data[1:2]
+#I cannot access rows in a dataframe with square brackets but I can access a range of rows (inclusive for the first number and exculisve 
+# for our second number) This would return only a single row. This also refers to the integer position of the row and not the actual index
+
+data[data['year'] > 1800]
+# return every row where the year is greater than 1800
+
+#data.loc[ROWS, COLS]
+#loc takes whichever rows we watn followed by whichever columns we want. 
+data.loc[0, :]
+#this would return the zeroth row and all the columns
+data.loc[0:2, :]
+#loc returns a range of rows that is inclusive on both ends. I will get three rows and all the columns
+data.loc[0:2, ['artist', 'title']]
+# this returns rows 0, 1, and 2 and the columns artist and title 
+data.loc[[1,5], "id":"artistId"]
+# this returns rows 1 and 5 and all columns from id to artistId
+data.loc[data.artist == 'Blake, Robert', :]
+# this returns every row where the artists name is 'Robert Blake' and every column
+#loc is label based so I have to specify rows and columns based on their labels, 
+#iloc is integer based so you have to specify rows and columns by their integer position values
